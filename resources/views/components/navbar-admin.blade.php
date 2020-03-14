d1fb6ed88dd05ad693ead28b76f650c983b12314
John Ferrer <johnrobert.ferrer.nelsoft@gmail.com>
2020-03-14 12:22:40 +0800

    :poop:

--- resources/views/components/navbar-admin.blade.php
+++ resources/views/components/navbar-admin.blade.php
@@ -28,15 +28,13 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
                                 <a class="dropdown-item" href="/">
                                     {{ __('Preview Home Page') }}
                                 </a>
-
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
